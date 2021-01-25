63d78a4908cac1af2a6bdab9e6337021c25cd2be
Zamir <zamir.zaripov.2001@mail.ru>
2021-01-25 15:36:06 +0500

    created console .net core project

---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace DzApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            //DZ-1
+
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            int s = 0;
+            for (int i = A + 1; i < B; i ++){
+                s += i;
+            }
+            Console.WriteLine(s);
+
+            int A1 = Convert.ToInt32(Console.ReadLine());
+            int B1 = Convert.ToInt32(Console.ReadLine());
+            for (int i = A1; i <= B1; i ++){
+                if (i % 2 != 0){
+                    Console.WriteLine(i);
+                }
+            }
+            
+            System.Console.Write("* * * * *");
+            System.Console.Write("\n");
+            System.Console.Write("*       *");
+            System.Console.Write("\n");
+            System.Console.Write("* * * * *");
+            System.Console.Write("\n");
+            System.Console.Write("\n");
+
+            System.Console.Write("*");
+            System.Console.Write("\n");
+            System.Console.Write("* *");
+            System.Console.Write("\n");
+            System.Console.Write("* * *");
+            System.Console.Write("\n");
+            System.Console.Write("\n");
+
+            System.Console.Write("    *");
+            System.Console.Write("\n");
+            System.Console.Write("  *   *");
+            System.Console.Write("\n");
+            System.Console.Write("*   *   *");
+            System.Console.Write("\n");
+            System.Console.Write("\n");
+
+            System.Console.Write("    *");
+            System.Console.Write("\n");
+            System.Console.Write("  *   *");
+            System.Console.Write("\n");
+            System.Console.Write("    *    ");
+            System.Console.Write("\n");
+            System.Console.Write("\n");
+
+            int p = Convert.ToInt32(Console.ReadLine());
+            int s1 = 1000, k = 0;
+            while(s1 <= 1100){
+                s1 += p;
+                k ++;
+            }
+            Console.WriteLine(k + " " + s1);
+
+            //DZ-2
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            int s2 = 0, maxi = -13876, mini = 13876;
+            for (int i = 0; i < n; i ++){
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                if(arr[i] > maxi){
+                    maxi = arr[i];
+                }
+                if(arr[i] < mini){
+                    mini = arr[i];
+                }
+                s2 += arr[i];
+            }
+            System.Console.WriteLine(maxi + " " + mini + " " + s2 + " " + (s2 / n));
+            for (int i = 0; i < n; i ++){
+                if(arr[i] % 2 != 0){
+                    System.Console.WriteLine(arr[i]);
+                }            
+            }
+
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n1];
+            for (int i = 0; i < n1; i ++){
+                array[i] = Convert.ToInt32(Console.ReadLine());            
+            }
+            for (int i = n1 - 1; i >= 0; i --){
+                System.Console.WriteLine(array[i]);;
+            }
+
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[N];
+            int count = Convert.ToInt32(Console.ReadLine());
+            int index = Convert.ToInt32(Console.ReadLine());
+            int[] b = new int[count];
+            for(int i = 0; i < N; i ++){
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i = 0; i < N; i ++){
+                Console.WriteLine(a[i]);
+            }
+            System.Console.WriteLine("\n");
+            int j = 0;
+            for(int i = index; i < N; i ++){
+                b[j] = a[i];
+                j ++;
+            }
+            for(int z = 0; z < j; z ++){
+                System.Console.WriteLine(b[z]);
+            }
+            if((N - index) < count){
+                for (int i = 0; i < (count - j); i ++){
+                    System.Console.WriteLine(1);
+                }
+            }
+        }
+    }
+}
