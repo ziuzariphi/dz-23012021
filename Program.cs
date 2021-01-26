19823bfba7e993a5af03ca2e3de9d54461dec8ba
Zamir <zamir.zaripov.2001@mail.ru>
2021-01-26 14:45:36 +0500

    message

--- Program.cs
+++ Program.cs
@@ -23,37 +23,51 @@ namespace DzApp
                     Console.WriteLine(i);
                 }
             }
+
+            System.Console.Write("\n");
+            for(int l = 0; l < 2; l ++){
+                for (int i = 0; i < 5; i ++){
+                    System.Console.Write("* ");
+                }
+                System.Console.Write("\n");
+            }
+            System.Console.Write("\n");
+
+            int r = 1;
+            while (r < 4){
+                for(int i = 0; i < r; i ++){
+                    System.Console.Write("* ");
+                }
+                System.Console.Write("\n");
+                r ++; 
+            }
+            System.Console.Write("\n");
             
-            System.Console.Write("* * * * *");
-            System.Console.Write("\n");
-            System.Console.Write("*       *");
-            System.Console.Write("\n");
-            System.Console.Write("* * * * *");
-            System.Console.Write("\n");
-            System.Console.Write("\n");
-
-            System.Console.Write("*");
-            System.Console.Write("\n");
-            System.Console.Write("* *");
-            System.Console.Write("\n");
-            System.Console.Write("* * *");
-            System.Console.Write("\n");
+            int t = 3, d = 2;
+            while (t > 0){
+                for (int i = 0; i < t; i ++){
+                    System.Console.Write("* ");
+                }
+                System.Console.Write("\n");
+                for (int i = 0; i < t - d; i ++){
+                    System.Console.Write(" ");
+                }
+                d -= 2;
+                t --;
+            }
             System.Console.Write("\n");
 
-            System.Console.Write("    *");
-            System.Console.Write("\n");
-            System.Console.Write("  *   *");
-            System.Console.Write("\n");
-            System.Console.Write("*   *   *");
-            System.Console.Write("\n");
-            System.Console.Write("\n");
-
-            System.Console.Write("    *");
-            System.Console.Write("\n");
-            System.Console.Write("  *   *");
-            System.Console.Write("\n");
-            System.Console.Write("    *    ");
-            System.Console.Write("\n");
+            for(int i = 0; i < 3; i ++){
+                if (i % 2 == 0){
+                    System.Console.Write("    *");
+                }
+                else{
+                    for(int x = 0; x < 2; x ++){
+                        System.Console.Write("  * ");
+                    }
+                }
+                System.Console.Write("\n");
+            }
             System.Console.Write("\n");
 
             int p = Convert.ToInt32(Console.ReadLine());
